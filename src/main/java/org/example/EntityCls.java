8a4c2db943d8afa43778de097e88dd3c456e5e00
krpatil5686 <krpatil5686@users.noreply.github.com>
2025-08-07 22:09:56 +0530

    first commit

---
+++ src/main/java/org/example/EntityCls.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EntityCls
+{
+    int id;
+    String name;
+    long salary;
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hi");
+
+    }
+}
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
